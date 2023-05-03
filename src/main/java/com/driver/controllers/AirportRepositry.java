46e6f6dfd94b95f7fc4c05ef916cafe397fff1c4
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-05-03 16:30:40 +0000

    check

--- src/main/java/com/driver/controllers/AirportRepositry.java
+++ src/main/java/com/driver/controllers/AirportRepositry.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.*;
 
 @Repository
-public class AirportRepository {
+class AirportRepository {
 
     TreeMap<String, Airport> airportDb = new TreeMap<>();
     HashMap<Integer, Flight> flightDb = new HashMap<>();
